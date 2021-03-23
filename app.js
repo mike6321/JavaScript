4706fa147cb6eb0ff760c50d285bdcf5b3ba848a
junwoo <33277588+mike6321@users.noreply.github.com>
2021-03-23 19:21:37 +0900

    Chapter01 - complete

---
+++ app.js
@@ -0,0 +1,4 @@
+import {product, sum} from './utility.js';
+
+console.log(product(1, 2));
+console.log(sum(1, 2)); 
---
+++ utility.js
@@ -0,0 +1,11 @@
+function sum(a, b) {
+    return a + b;
+}
+
+function product(a, b) {
+    return a * b;
+}
+export {
+    product,
+    sum
+}
